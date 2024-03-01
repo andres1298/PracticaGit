6000522421639c5496a432a9fe9ecd5e9081c087
AndresB <andres.bonilla1298@gmail.com>
2024-02-29 18:56:21 -0600

    Agregar condicionales al flujo del programa

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,11 +8,17 @@ print("Bienvenido a Empresas Camisuchis")
 salario_bruto = int(input("Ingrese el salario bruto del empleado: "))
 camisas_vendidas = int(input("Ingrese la cantidad de camisas vendidas: "))
 
-seguro_salud = salario_bruto * 0.08
-impuesto_renta = salario_bruto * 0.12
-costo_camisa = 15000
-regalias = (costo_camisa * camisas_vendidas) * 0.03
+if salario_bruto > 1000000:
+    seguro_salud = salario_bruto * 0.08
+    impuesto_renta = salario_bruto * 0.12
+    costo_camisa = 15000
+    regalias = (costo_camisa * camisas_vendidas) * 0.03
+else:
+    seguro_salud = salario_bruto * 0.08
+    impuesto_renta = salario_bruto * 0.12
+    costo_camisa = 15000
+    regalias = (costo_camisa * camisas_vendidas) * 0.03
+
 
 salario_neto = salario_bruto - seguro_salud - impuesto_renta + regalias
-print(salario_bruto, seguro_salud, impuesto_renta, regalias)
 print(f"El salario total del empleado es: {salario_neto}")
