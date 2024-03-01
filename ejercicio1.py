250bbac0ed65f93dc949996ac7879a6f492c65b3
AndresB <andres.bonilla1298@gmail.com>
2024-02-29 18:48:42 -0600

    Primer commit. Agregar archivo ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,18 @@
+"""
+Empresas Camisuchis
+Ejercicio No.1
+Caculo de salario neto de empleado
+"""
+
+print("Bienvenido a Empresas Camisuchis")
+salario_bruto = int(input("Ingrese el salario bruto del empleado: "))
+camisas_vendidas = int(input("Ingrese la cantidad de camisas vendidas: "))
+
+seguro_salud = salario_bruto * 0.08
+impuesto_renta = salario_bruto * 0.12
+costo_camisa = 15000
+regalias = (costo_camisa * camisas_vendidas) * 0.03
+
+salario_neto = salario_bruto - seguro_salud - impuesto_renta + regalias
+print(salario_bruto, seguro_salud, impuesto_renta, regalias)
+print(f"El salario total del empleado es: {salario_neto}")
